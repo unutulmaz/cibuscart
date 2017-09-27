27fe043c2629bdbf74ebaf65d14f0c92c857e0d1
Brian Lusina <lusinabrian@gmail.com>
2017-09-27 21:13:49 +0300

    Update docker compose file

--- setup-docker.sh
+++ setup-docker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # build the flask container
-docker build -t prakhar1989/foodtrucks-web .
+docker build -t thelusina/cibuscart .
 
 # create the network
-docker network create foodtrucks
+docker network create cibuscart
 
 # start the ES container
-docker run -d --net foodtrucks -p 9200:9200 -p 9300:9300 --name es elasticsearch
+docker run -d --net cibuscart -p 9200:9200 -p 9300:9300 --name es elasticsearch
 
 # start the flask app container
-docker run -d --net foodtrucks -p 5000:5000 --name foodtrucks-web prakhar1989/foodtrucks-web
+docker run -d --net cibuscart -p 5000:5000 --name cibuscart-server thelusina/cibuscart
+
+docker run -d --net cibuscart -p 3000:3000 --name cibuscart-client thelusina/cibuscart
